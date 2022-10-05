482e569bed98a114fded6e63f844ea283410cbe2
kw-shakshi3104 <103113049+kw-shakshi3104@users.noreply.github.com>
2022-10-05 21:00:44 +0900

    Initial Commit

---
+++ DIG-components/DIG_componentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIG_componentsApp.swift
+//  DIG-components
+//
+//  Created by 小林慧 on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct DIG_componentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
